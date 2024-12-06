f06c49859f9bf2ca49dbeccf84ab5211886b0932
Pablo Lobos <nodevice@gmail.com>
2024-12-05 23:08:36 -0300

    tailwind

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Sofia_Sans } from "next/font/google";
 import { draftMode } from "next/headers";
 import { VisualEditing, toPlainText } from "next-sanity";
 import { Toaster } from "sonner";
@@ -51,11 +51,12 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-const inter = Inter({
-  variable: "--font-inter",
+const sofiaSans = Sofia_Sans({
+  variable: "--font-sofia-sans",
   subsets: ["latin"],
   display: "swap",
 });
+
 
 export default async function RootLayout({
   children,
@@ -65,7 +66,7 @@ export default async function RootLayout({
   const { isEnabled: isDraftMode } = await draftMode();
 
   return (
-    <html lang="es" className={`${inter.variable} bg-white text-black`}>
+    <html lang="es" className={` ${sofiaSans.variable} bg-white text-black`}>
       <body>
         <section className="pt-24 min-h-screen">
           {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
