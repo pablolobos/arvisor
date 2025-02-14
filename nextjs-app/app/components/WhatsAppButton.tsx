991c76ec40b86bbb3794a3b81798ae3dd583b920
Pablo Lobos <nodevice@gmail.com>
2025-02-14 09:57:51 -0300

    detalles

--- nextjs-app/app/components/WhatsAppButton.tsx
+++ nextjs-app/app/components/WhatsAppButton.tsx
@@ -15,11 +15,11 @@ export default function WhatsAppButton({ phoneNumber }: WhatsAppButtonProps) {
             href={whatsappUrl}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex relative items-center gap-2 md:gap-6 bg-brand-purpleHighlight hover:bg-green-600 py-3 rounded-full font-semibold text-white text-base md:text-2xl transition-colors duration-200"
+            className="inline-flex relative items-center gap-2 md:gap-6 bg-brand-purpleHighlight hover:bg-green-600 py-3 rounded-full font-semibold text-white text-base md:text-xl transition-colors duration-200"
         >
 
             <span className='pl-3 md:pl-6'>Hablemos</span>
-            <Image src="/whatsapp.svg" alt="WhatsApp" width={40} height={40} className='md:flex-shrink-0 mr-3 md:mr-6' />
+            <Image src="/whatsapp.svg" alt="WhatsApp" width={30} height={30} className='md:flex-shrink-0 mr-3 md:mr-6' />
         </a>
     );
 } 
